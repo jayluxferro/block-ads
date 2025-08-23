d9a4a92947046ab708ad792a359a562fccf926e7
Jay <jay@sperixlabs.org>
2025-08-23 20:10:48 +0000

    Sat Aug 23 20:10:48 GMT 2025

--- extra_list/format_list.py
+++ extra_list/format_list.py
@@ -5,6 +5,13 @@ output_file = "hosts.txt"
 input_file = "combined_list.txt"
 
 domains = []
+excluded_list = [
+    "localhost",
+    "advertising",
+    "tracker",
+    "https://github.com/stevenblack/hosts/issues/1635",
+    "redirects",
+]
 
 with open(input_file, "r") as in_file:
     for line in in_file:
@@ -12,7 +19,7 @@ with open(input_file, "r") as in_file:
         if not line or line.startswith("#"):
             continue
         line = line.split(" ")[-1].lower()
-        if line == "localhost":
+        if line in excluded_list:
             continue
 
         domains.append(f"0.0.0.0\t{line}")
